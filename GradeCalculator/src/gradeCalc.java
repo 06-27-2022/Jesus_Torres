dbc2c488422f0f93dd460e9e66d6de3dd4d475e3
1jestor1 <jtorres122436@gmail.com>
2022-06-29 16:41:29 -0500

    First version uploaded. There is probably a way to use an enhanced loop but I do not know how to do that using Java.

---
+++ GradeCalculator/src/gradeCalc.java
@@ -0,0 +1,10 @@
+
+public class gradeCalc {
+	public static void main(String[] args) {
+		int[] grades = {95, 85, 62, 40, 79};
+		for(int i:grades) {
+			System.out.println(i);
+			System.out.println(grades[i]);
+		}
+	}
+}
