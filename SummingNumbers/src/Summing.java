d156b315a2312c9a8501520af41b0d0533963130
1jestor1 <jtorres122436@gmail.com>
2022-06-30 18:05:48 -0500

    Added comments to my code.

--- SummingNumbers/src/Summing.java
+++ SummingNumbers/src/Summing.java
@@ -1,6 +1,6 @@
 
 public class Summing {
-	
+	//adds numbers in an array regardless of length.
 	int bgsum(int[] arr) {
 		int rslt=0;
 		for(int num: arr) {
@@ -9,10 +9,19 @@ public class Summing {
 		return rslt;
 	}
 	public static void main(String[] args) {
+		//Example arrays to apply method.
 		int[] arr0 = {3, 4, 332, 4, 3, 222, 402, 8392, 83, 2, 1, 244, 5, 4, 33, 842, 0, 0, 0, 2, 3, 4, 0, 8871, 3, 1, 7, 9, 900, 1, 19, 23, 98, 89, 733, 437, 87, 98};
 		int[] arr1 = {};
 		int[] arr2 = {3,2,3};
 		int[] arr3 = {1};
+		//There doesn't seem to be a way to apply a method
+		//directly to an array so I created an empty instance
+		//to apply my method to in order to apply my method
+		//to my array as an argument in my method.
+		//Feels inelegant and that there should be a better
+		//method. 
+		//Also this comment should have used the alternate
+		//formatting.
 		Summing plchldr = new Summing();
 		
 		System.out.println(plchldr.bgsum(arr0));
